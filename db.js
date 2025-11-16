16eeedea933051223026d1c48e4eb7b5e23db646
Eleanor <eleanortien@gmail.com>
2025-11-16 00:00:46 -0500

    init oracle connection

--- db.js
+++ db.js
@@ -1,43 +1,79 @@
+require('dotenv').config();
 const oracledb = require('oracledb');
+
+// Initialize Thick mode
+try {
+    oracledb.initOracleClient();
+    console.log('node-oracledb running in Thick mode.');
+} catch (err) {
+    console.error('Error initializing Oracle Client:', err);
+    process.exit(1); // Exit if initialization fails
+}
+
 async function runApp()
 {
-  let connection;
-  try {
-    connection = await oracledb.getConnection({ user: "demonode", password: "XXXX", connectionString: "localhost/xepdb1" });
-    console.log("Successfully connected to Oracle Database");
-  
-    // Create a table
-    await connection.execute(`begin execute immediate 'drop table todoitem'; exception when others then if sqlcode <> -942 then raise; end if; end;`);
-    await connection.execute(`create table todoitem ( id number generated always as identity, description varchar2(4000), creation_ts timestamp with time zone default current_timestamp, done number(1,0), primary key (id))`);
-  
-    // Insert some data
-    const sql = `insert into todoitem (description, done) values(:1, :2)`;
-    const rows = [ ["Task 1", 0 ], ["Task 2", 0 ], ["Task 3", 1 ], ["Task 4", 0 ], ["Task 5", 1 ] ];
-    let result = await connection.executeMany(sql, rows);
-    console.log(result.rowsAffected, "Rows Inserted");
-    connection.commit();
-  
-    // Now query the rows back
-    result = await connection.execute( `select description, done from todoitem`, [], { resultSet: true, outFormat: oracledb.OUT_FORMAT_OBJECT });
-    const rs = result.resultSet; let row;
-    while ((row = await rs.getRow())) {
-      if (row.DONE)
-        console.log(row.DESCRIPTION, "is done");
-      else
-        console.log(row.DESCRIPTION, "is NOT done");
-    }
-    await rs.close();
-  } catch (err) {
-    console.error(err);
-  } finally {
-    if (connection)
-    {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  }
+    config = 
+    {
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        connectionString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))",
+    }
+    console.log(config);
+    let connection;
+    try {
+        connection = await oracledb.getConnection(config);
+        console.log("Successfully connected to Oracle Database");
+
+        // Create a table
+        await connection.execute(`
+        BEGIN
+            EXECUTE IMMEDIATE 'DROP TABLE todoitem';
+        EXCEPTION
+            WHEN OTHERS THEN
+            IF SQLCODE != -942 THEN
+                RAISE;
+            END IF;
+        END;`,
+        [],
+        { autoCommit: true });
+       await connection.execute(`
+            CREATE TABLE todoitem (
+                id           NUMBER,
+                description  VARCHAR2(4000),
+                creation_ts  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                done         NUMBER(1,0),
+                PRIMARY KEY (id)
+            )`,
+            [],
+        { autoCommit: true });
+        // Insert some data
+        const sql = `insert into todoitem (id, description, done) values(:1, :2, :3)`;
+        const rows = [ [0, "Task 1", 0 ], [1, "Task 2", 0 ], [2, "Task 3", 1 ], [3, "Task 4", 0 ], [4, "Task 5", 1 ] ];
+        let result = await connection.executeMany(sql, rows);
+        console.log(result.rowsAffected, "Rows Inserted");
+        connection.commit();
+        
+        // Now query the rows back
+        result = await connection.execute( `select description, done from todoitem`, [], { resultSet: true, outFormat: oracledb.OUT_FORMAT_OBJECT });
+        const rs = result.resultSet; let row;
+        while ((row = await rs.getRow())) {
+            if (row.DONE)
+                console.log(row.DESCRIPTION, "is done");
+            else
+                console.log(row.DESCRIPTION, "is NOT done");
+        }
+        await rs.close();
+    } catch (err) {
+        console.error(err);
+    } finally {
+        if (connection)
+        {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
 }
 runApp();
